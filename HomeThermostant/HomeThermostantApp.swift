7d7c2c5363813d1ae06e5bf9938d4d6ca0c31035
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-04-25 21:50:32 -0400

    Initial Commit

---
+++ HomeThermostant/HomeThermostantApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeThermostantApp.swift
+//  HomeThermostant
+//
+//  Created by Furkan Adıgüzel on 2022-04-25.
+//
+
+import SwiftUI
+
+@main
+struct HomeThermostantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
